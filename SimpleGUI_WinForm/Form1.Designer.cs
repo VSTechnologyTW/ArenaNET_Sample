d93bc19644cc265f20ab9b8c012498ca784f49b0
Jason <jason2677350@gmail.com>
2019-01-25 12:44:19 +0800

    Add SimpleGUI_WinForm project

---
+++ SimpleGUI_WinForm/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace SimpleGUI_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ConsoleView = new System.Windows.Forms.TextBox();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.InitializeButton = new System.Windows.Forms.Button();
+            this.GrabbedImage = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GrabbedImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 350F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.GrabbedImage, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(782, 403);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.ConsoleView);
+            this.panel1.Controls.Add(this.CloseButton);
+            this.panel1.Controls.Add(this.OpenButton);
+            this.panel1.Controls.Add(this.SearchButton);
+            this.panel1.Controls.Add(this.InitializeButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(344, 397);
+            this.panel1.TabIndex = 0;
+            // 
+            // ConsoleView
+            // 
+            this.ConsoleView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ConsoleView.Location = new System.Drawing.Point(0, 124);
+            this.ConsoleView.Multiline = true;
+            this.ConsoleView.Name = "ConsoleView";
+            this.ConsoleView.ReadOnly = true;
+            this.ConsoleView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ConsoleView.Size = new System.Drawing.Size(344, 273);
+            this.ConsoleView.TabIndex = 4;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.AutoSize = true;
+            this.CloseButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.CloseButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.CloseButton.Enabled = false;
+            this.CloseButton.Location = new System.Drawing.Point(0, 93);
+            this.CloseButton.Margin = new System.Windows.Forms.Padding(5);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Padding = new System.Windows.Forms.Padding(3);
+            this.CloseButton.Size = new System.Drawing.Size(344, 31);
+            this.CloseButton.TabIndex = 3;
+            this.CloseButton.Text = "4.Close Device";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.AutoSize = true;
+            this.OpenButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.OpenButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.OpenButton.Enabled = false;
+            this.OpenButton.Location = new System.Drawing.Point(0, 62);
+            this.OpenButton.Margin = new System.Windows.Forms.Padding(5);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Padding = new System.Windows.Forms.Padding(3);
+            this.OpenButton.Size = new System.Drawing.Size(344, 31);
+            this.OpenButton.TabIndex = 2;
+            this.OpenButton.Text = "3.Open First Device, grab 10 photos";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.AutoSize = true;
+            this.SearchButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.SearchButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.SearchButton.Enabled = false;
+            this.SearchButton.Location = new System.Drawing.Point(0, 31);
+            this.SearchButton.Margin = new System.Windows.Forms.Padding(5);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Padding = new System.Windows.Forms.Padding(3);
+            this.SearchButton.Size = new System.Drawing.Size(344, 31);
+            this.SearchButton.TabIndex = 1;
+            this.SearchButton.Text = "2.Search Device";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // InitializeButton
+            // 
+            this.InitializeButton.AutoSize = true;
+            this.InitializeButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.InitializeButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.InitializeButton.Location = new System.Drawing.Point(0, 0);
+            this.InitializeButton.Margin = new System.Windows.Forms.Padding(5);
+            this.InitializeButton.Name = "InitializeButton";
+            this.InitializeButton.Padding = new System.Windows.Forms.Padding(3);
+            this.InitializeButton.Size = new System.Drawing.Size(344, 31);
+            this.InitializeButton.TabIndex = 0;
+            this.InitializeButton.Text = "1.Initialize";
+            this.InitializeButton.UseVisualStyleBackColor = true;
+            this.InitializeButton.Click += new System.EventHandler(this.InitializeButton_Click);
+            // 
+            // GrabbedImage
+            // 
+            this.GrabbedImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GrabbedImage.Location = new System.Drawing.Point(353, 3);
+            this.GrabbedImage.Name = "GrabbedImage";
+            this.GrabbedImage.Size = new System.Drawing.Size(426, 397);
+            this.GrabbedImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.GrabbedImage.TabIndex = 1;
+            this.GrabbedImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 403);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GrabbedImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button InitializeButton;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.TextBox ConsoleView;
+        private System.Windows.Forms.PictureBox GrabbedImage;
+    }
+}
+
--- SimpleGUI_WinForm/Form1.cs
+++ SimpleGUI_WinForm/Form1.cs
@@ -1,34 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Media.Media3D;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 using ArenaNET;
 
-namespace SimpleGUI_WPF
+namespace SimpleGUI_WinForm
 {
     /// <summary>
-    /// This example demonstrates the integration of a camera with a GUI application (WPF framework).
+    /// This example demonstrates the integration of a camera with a GUI application (Windows Form framework).
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Form1 : Form
     {
+
         public IDevice Device { get; set; }
+
         public ISystem ArenaNETSystem { get; set; }
 
         public CancellationTokenSource ImagePollingCancellationTokenSource { get; set; }
+
         public Task PollingTask { get; set; }
+
         public int FrameRate { get; set; } = 10;
+
         public int PollingImageTimeout => (1000 / FrameRate) + 200;
 
         public int FrameCount { get; set; } = 10;
 
-        public MainWindow()
+        public Form1()
         {
             InitializeComponent();
-            GrabbedImage.Source = new WriteableBitmap(800, 600, 96, 96, PixelFormats.Bgr24, null);
             ImagePollingCancellationTokenSource = new CancellationTokenSource();
         }
 
@@ -45,6 +53,58 @@ namespace SimpleGUI_WPF
             (device.NodeMap.GetNode("AcquisitionFrameRate") as IFloat).Value = FrameRate;
         }
 
+        private void UpdateConsoleView(object sender, string str)
+        {
+            if (InvokeRequired)
+            {
+                // If called from a different thread, we must use the Invoke method to marshal the call to the proper thread.
+                BeginInvoke(new EventHandler<string>(UpdateConsoleView), sender, str);
+                return;
+            }
+
+            var textbox = sender as TextBox;
+            if (textbox != null)
+            {
+                if (string.IsNullOrEmpty(str))
+                {
+                    textbox.Text = string.Empty;
+                }
+                else
+                {
+                    textbox.Text += str;
+                }
+            }
+        }
+
+        private void UpdateImage(object sender, IImage image)
+        {
+            if (InvokeRequired)
+            {
+                // If called from a different thread, we must use the Invoke method to marshal the call to the proper thread.
+                BeginInvoke(new EventHandler<IImage>(UpdateImage), sender, image);
+                return;
+            }
+
+            UpdateConsoleView(ConsoleView, $"Image Grabbed({image.FrameId}). - {DateTime.Now}" + Environment.NewLine);
+
+            // Release resources
+            if (GrabbedImage.Image != null)
+            {
+                GrabbedImage.Image.Dispose();
+                ArenaNET.ImageFactory.Destroy(GrabbedImage.Tag as IImage);
+            }
+
+            // Convert image to rgb24 format
+            var bgr24Image = ArenaNET.ImageFactory.Convert(image, EPfncFormat.BGR8);
+
+            // Set image
+            GrabbedImage.Image = bgr24Image.Bitmap;
+            GrabbedImage.Tag = bgr24Image;
+
+            // Release source image
+            ArenaNET.ImageFactory.Destroy(image);
+        }
+
         private Task StartPollingImages(CancellationTokenSource cancellationTokenSource)
         {
             return Task.Run(() =>
@@ -56,10 +116,7 @@ namespace SimpleGUI_WPF
                     {
                         if (!Device.IsConnected)
                         {
-                            Dispatcher.InvokeAsync(() =>
-                            {
-                                ConsoleView.Text += $"Camera Disconnected. - {DateTime.Now}" + Environment.NewLine;
-                            });
+                            UpdateConsoleView(ConsoleView, $"Camera Disconnected. - {DateTime.Now}" + Environment.NewLine);
                             break;
                         }
 
@@ -71,10 +128,7 @@ namespace SimpleGUI_WPF
                         if (image.IsIncomplete)
                         {
                             Device.RequeueBuffer(image);
-                            Dispatcher.InvokeAsync(() =>
-                            {
-                                ConsoleView.Text += $"Missing data. - {DateTime.Now}" + Environment.NewLine;
-                            });
+                            UpdateConsoleView(ConsoleView, $"Missing data. - {DateTime.Now}" + Environment.NewLine);
                             continue;
                         }
 
@@ -85,29 +139,8 @@ namespace SimpleGUI_WPF
                         // ===========================================
                         // Do Something
 
-                        // Show image , using BGR8 (BGR24)
-                        Dispatcher.InvokeAsync(() =>
-                        {
-                            ConsoleView.Text += $"Image Grabbed({copyImage.FrameId}). - {DateTime.Now}" +
-                                                Environment.NewLine;
-                            var wb = GrabbedImage.Source as WriteableBitmap;
-                            if (wb == null || wb.PixelWidth != copyImage.Width || wb.PixelHeight != copyImage.Height)
-                            {
-                                GrabbedImage.Source = new WriteableBitmap((int)copyImage.Width, (int)copyImage.Height,
-                                    96, 96, PixelFormats.Bgr24, null);
-                                wb = GrabbedImage.Source as WriteableBitmap;
-                            }
-
-                            var bgr24Image = ArenaNET.ImageFactory.Convert(copyImage, EPfncFormat.BGR8);
-                            wb.Lock();
-                            Marshal.Copy(bgr24Image.DataArray, 0, wb.BackBuffer, bgr24Image.DataArray.Length);
-                            wb.AddDirtyRect(new Int32Rect(0, 0, wb.PixelWidth, wb.PixelHeight));
-                            wb.Unlock();
-
-                            // Remember to release the resources
-                            ArenaNET.ImageFactory.Destroy(copyImage);
-                            ArenaNET.ImageFactory.Destroy(bgr24Image);
-                        });
+                        // Show image
+                        UpdateImage(GrabbedImage, copyImage);
                         image_count++;
                         if (image_count == FrameCount)
                             break;
@@ -131,20 +164,20 @@ namespace SimpleGUI_WPF
 
         #region UI Event
 
-        private void InitializeButton_OnClick(object sender, RoutedEventArgs e)
+        private void InitializeButton_Click(object sender, EventArgs e)
         {
             try
             {
                 // Clear hte ConsoleView
                 ConsoleView.Text = "";
-                
+
                 // Open system
                 ArenaNETSystem = ArenaNET.Arena.OpenSystem();
 
                 // Update information
                 ConsoleView.Text += $"ArenaNETSystem already opened. - {DateTime.Now}" + Environment.NewLine;
-                InitializeButton.IsEnabled = false;
-                SearchButton.IsEnabled = true;
+                InitializeButton.Enabled = false;
+                SearchButton.Enabled = true;
             }
             catch (Exception exception)
             {
@@ -152,25 +185,24 @@ namespace SimpleGUI_WPF
             }
         }
 
-        private void SearchButton_OnClick(object sender, RoutedEventArgs e)
+        private void SearchButton_Click(object sender, EventArgs e)
         {
             try
             {
                 // Update device list
                 if (ArenaNETSystem.UpdateDevices(1000))
                 {
-                    // if at least one device found, open it
                     if (ArenaNETSystem.Devices.Count != 0)
                     {
                         // if at least one device found, open it
                         Device = ArenaNETSystem.CreateDevice(ArenaNETSystem.Devices[0]);
-                        
+
                         // Update information
                         ConsoleView.Text += $"{ArenaNETSystem.Devices.Count } devices found. - {DateTime.Now}" + Environment.NewLine;
                         ConsoleView.Text += $"First camera opened. - {DateTime.Now}" + Environment.NewLine;
 
-                        SearchButton.IsEnabled = false;
-                        OpenButton.IsEnabled = true;
+                        SearchButton.Enabled = false;
+                        OpenButton.Enabled = true;
                     }
                     else
                     {
@@ -188,13 +220,13 @@ namespace SimpleGUI_WPF
             }
         }
 
-        private void OpenButton_OnClick(object sender, RoutedEventArgs e)
+        private void OpenButton_Click(object sender, EventArgs e)
         {
             try
             {
                 // Set up camera
                 SetUpCamera(Device);
-                
+
                 // Start polling task, polling image from different thread
                 ImagePollingCancellationTokenSource = new CancellationTokenSource();
                 PollingTask = StartPollingImages(ImagePollingCancellationTokenSource);
@@ -202,8 +234,8 @@ namespace SimpleGUI_WPF
                 // Start stream
                 Device.StartStream();
 
-                OpenButton.IsEnabled = false;
-                CloseButton.IsEnabled = true;
+                OpenButton.Enabled = false;
+                CloseButton.Enabled = true;
             }
             catch (Exception exception)
             {
@@ -211,7 +243,7 @@ namespace SimpleGUI_WPF
             }
         }
 
-        private void CloseButton_OnClick(object sender, RoutedEventArgs e)
+        private void CloseButton_Click(object sender, EventArgs e)
         {
             try
             {
@@ -226,8 +258,8 @@ namespace SimpleGUI_WPF
                 ArenaNETSystem.DestroyDevice(Device);
                 ArenaNET.Arena.CloseSystem(ArenaNETSystem);
 
-                InitializeButton.IsEnabled = true;
-                CloseButton.IsEnabled = false;
+                InitializeButton.Enabled = true;
+                CloseButton.Enabled = false;
             }
             catch (Exception exception)
             {
